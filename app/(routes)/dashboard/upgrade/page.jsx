a8e602bbc1fb28102a80dcf8502033281b6beae0
Sriya0001 <kunisettysriya@gmail.com>
2025-08-09 10:26:00 +0530

    Add Upgrade page to dashboard

---
+++ app/(routes)/dashboard/upgrade/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Upgrade() {
+  return (
+    <div className="p-10">
+      <h2 className="font-bold text-3xl">
+        You can see the latest upgrades here.
+      </h2>
+    </div>
+  );
+}
+
+export default Upgrade;
